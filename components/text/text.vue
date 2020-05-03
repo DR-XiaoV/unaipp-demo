ddeca1a7890dc2542f35d74c7665d8c7c541b684
unknown <2683926960@qq.com>
2020-05-03 21:55:50 +0800

    first

---
+++ components/text/text.vue
@@ -0,0 +1,30 @@
+<template>
+	<view>
+		<text>这是一个text组件</text>
+		<text>
+			{{msg}}
+		</text>
+		<button type="primary" @tap="test">event</button>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			};
+		},
+		props:["msg"],
+		methods: {
+			test(){
+				// this.$emit("showName",{name: "chujian"})
+				uni.$emit("showAge",{age: 20})
+			}
+		}
+	}
+</script>
+
+<style lang="scss">
+
+</style>
---
+++ pages/index/index.vue
@@ -0,0 +1,58 @@
+<template>
+	<view class="content">
+		<view class="text-area">
+			<test :msg="msg" @showName="testEvent"></test>
+			<text class="title">{{title}}</text>
+		</view>
+		<view>
+			{{"LILI"}}
+		</view>
+		{{name}}
+		<view>
+			{{"Milk.indexOf(lk)" !== -1 ? "chujian" : "galaxy"}}
+		</view>
+		<view 
+			v-for="item,index in nameList" 
+			:key="index"
+			:style="[{width: '100px', height: '100px', background: 'pink'}]"
+		>
+			{{item}}
+		</view>
+	</view>
+	
+</template>
+
+<script>
+	import test from '../../components/text/text.vue'
+	export default {
+		
+		data() {
+			return {
+				title: 'Hello',
+				msg: "silver river",
+				name: "lili",
+				nameList: ['12','23','34','45']
+			}
+		},
+		components: {
+			test 
+		},
+		onLoad() {
+			// uni.$on("showAge",(rel)=>{
+			// 	console.log(rel)
+			// })
+			uni.$once("showAge",(rel)=>{
+				console.log(rel)
+			})
+		},
+		methods: {
+			testEvent(name){
+				console.log(name.name || name)
+			}
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+	
+</style>
